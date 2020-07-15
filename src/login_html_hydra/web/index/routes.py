0e66124ba04189530d574132d04f7aef9d635195
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-07-15 18:20:21 -0300

    elimine el codfigo que estaba demas

--- src/login_html_hydra/web/index/routes.py
+++ src/login_html_hydra/web/index/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect,request, Markup, url_for
 from .bp import bp
 from . import config
 
-from login_html_hydra import config
+
 
 @bp.route('/')
 def index():
