78c98f13f37b46a220acc34df1a23a6eb1e32fb1
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-07-16 09:49:22 -0300

    elimino el remember asi walter no se confunde

--- src/login_html_hydra/web/login/forms.py
+++ src/login_html_hydra/web/login/forms.py
@@ -6,5 +6,5 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Usuario', validators=[DataRequired()])
     password = PasswordField('Clave', validators=[DataRequired()])
-    remember_me = BooleanField('Recordar')
+    #remember_me = BooleanField('Recordar')
     submit = SubmitField('Ingresar')
