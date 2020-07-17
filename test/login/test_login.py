1eabc459319218e0920f4209b02e49f3b3610a03
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-07-17 20:23:26 -0300

    agerego un ejemplo de test

--- test/login/test_login.py
+++ test/login/test_login.py
@@ -1,3 +1,14 @@
+import pytest
+import requests
+
+login_url = 'http://localhost:5000/login/'
 
 def test_login_ok():
-    raise Exception()
+    params = {
+        'username': 'a',
+        'password': '',
+        'challenge': 'challenge no valido'
+    }
+    r = requests.post(login_url, params, allow_redirects=False)
+    pytest.fail(str(r.status_code))
+    
