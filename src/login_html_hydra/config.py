f2ca37f26b60816d261d44ca427d59bb00ebf23a
Miguel Macagno <miguel.macagno@gmail.com>
2020-07-17 19:45:17 -0300

    correccion de variables de db

--- src/login_html_hydra/config.py
+++ src/login_html_hydra/config.py
@@ -7,15 +7,15 @@ class UserEnv:
     DB_USER = os.environ.get('USERS_DB_USER')
     DB_PASSWORD = os.environ.get('USERS_DB_PASSWORD')
     DB_HOST = os.environ.get('USERS_DB_HOST')
-    DB_PORT = os.environ.get('USERS_DB_PORT',5432)    
+    DB_PORT = os.environ.get('USERS_DB_PORT',5432)
     DB_NAME = os.environ.get('USERS_DB_NAME')
 
 #CONFIG DE LOGIN
 class LoginEnv:
-    DB_USER = os.environ['DB_USER'],
-    DB_PASSWORD = os.environ['DB_PASSWORD'],
-    DB_HOST = os.environ['DB_HOST'],
-    DB_PORT = os.environ.get('DB_PORT', 5432),
+    DB_USER = os.environ['DB_USER']
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+    DB_HOST = os.environ['DB_HOST']
+    DB_PORT = os.environ.get('DB_PORT',5432)
     DB_NAME = os.environ['DB_NAME']
 
 class HydraEnv:
--- src/login_html_hydra/models/db.py
+++ src/login_html_hydra/models/db.py
@@ -7,7 +7,6 @@ from login_html_hydra.config import UserEnv, LoginEnv
 @contextlib.contextmanager
 def open_session(dbhost, dbport, dbname, dbuser, dbpassword, echo=True,):
     engine = create_engine(f'postgresql://{dbuser}:{dbpassword}@{dbhost}:{dbport}/{dbname}', echo=echo)
-
     Session = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
     session = Session()
     try:
@@ -19,18 +18,20 @@ def open_session(dbhost, dbport, dbname, dbuser, dbpassword, echo=True,):
 
 @contextlib.contextmanager
 def open_users_session():
-    dbuser = UserEnv.DB_USER,
-    dbpassword = UserEnv.DB_PASSWORD,
+    dbuser = UserEnv.DB_USER
+    dbpassword = UserEnv.DB_PASSWORD
     dbhost = UserEnv.DB_HOST
     dbport = UserEnv.DB_PORT
     dbname = UserEnv.DB_NAME
-    yield open_session(dbhost, dbport, dbname, dbuser, dbpassword)
+    with open_session(dbhost, dbport, dbname, dbuser, dbpassword) as session:
+        yield session
 
 @contextlib.contextmanager
 def open_login_session():
-    dbuser = LoginEnv.DB_USER,
-    dbpassword = LoginEnv.DB_PASSWORD,
+    dbuser = LoginEnv.DB_USER
+    dbpassword = LoginEnv.DB_PASSWORD
     dbhost = LoginEnv.DB_HOST
     dbport = LoginEnv.DB_PORT
     dbname = LoginEnv.DB_NAME
-    yield open_session(dbhost, dbport, dbname, dbuser, dbpassword)
+    with open_session(dbhost, dbport, dbname, dbuser, dbpassword) as session:
+        yield session
