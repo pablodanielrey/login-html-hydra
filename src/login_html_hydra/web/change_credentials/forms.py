71709ea08d62fac1911828fe1fba641d6e79e2e4
Walter Blanco <walter@creativehands.com.ar>
2020-07-22 19:55:43 -0300

    Rearme y estile las pantallas de recuperar contraseña y contraseña temporal con sus respectivas pantallas (falta hacer el estilado fino).

--- src/login_html_hydra/web/change_credentials/forms.py
+++ src/login_html_hydra/web/change_credentials/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 class ChangeCredentialsForm(FlaskForm):
     password = PasswordField('Clave Actual', validators=[DataRequired()])
-    password2 = PasswordField('Clave Nueva', validators=[DataRequired()])
-    password2_confirmation = PasswordField('Confirmar Clave Nueva', validators=[DataRequired()])
-    submit = SubmitField('Aceptar')
+    password2 = PasswordField('Nueva Contraseña', validators=[DataRequired()])
+    password2_confirmation = PasswordField('Repetir Contraseña', validators=[DataRequired()])
+    submit = SubmitField('CONTINUAR')
--- src/login_html_hydra/web/reset_credentials/forms.py
+++ src/login_html_hydra/web/reset_credentials/forms.py
@@ -5,14 +5,14 @@ from wtforms.validators import DataRequired
 
 class InputCode(FlaskForm):
     code = StringField('Código', validators=[DataRequired()])
-    submit = SubmitField('Aceptar')
+    submit = SubmitField('CONTINUAR')
 
 
 class InputCredentials(FlaskForm):
-    password = PasswordField('Clave', validators=[DataRequired()])
-    password2 = PasswordField('Reconfirmar clave', validators=[DataRequired()])
-    submit = SubmitField('Aceptar')
+    password = PasswordField('Nueva Contraseña', validators=[DataRequired()])
+    password2 = PasswordField('Repetir Contraseña', validators=[DataRequired()])
+    submit = SubmitField('CONTINUAR')
 
 class InputUsername(FlaskForm):
-    username = StringField('DNI', validators=[DataRequired()])
-    submit = SubmitField('Aceptar')
+    username = StringField('Ingrese su DNI', validators=[DataRequired()])
+    submit = SubmitField('CONTINUAR')
