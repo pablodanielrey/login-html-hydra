46bfc367e103e9eb95d9858eace7e18cac786fee
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-07-28 17:08:24 -0300

    agregamos para que los chicos tengan los casos de testing

--- tests/functional/credentials/test_username.py
+++ tests/functional/credentials/test_username.py
@@ -1,7 +1,21 @@
 
 
-def test_get_username():
-    pass
+def test_get_username(client):
+    r = client.get('/reset_credentials/username')
+    assert r.status_code == 200
+    assert 'Ingrese su DNI' in str(r.data)
+    assert 'formulario' in str(r.data)
 
-def test_post_username():
-    pass
+
+def test_null_username_post(client):
+    r = client.post('/reset_credentials/username')
+    assert r.status_code == 400
+    assert 'formulario' in str(r.data)
+ 
+def test_ok_username_post(config, client):
+    params = {
+        'username': config['credentials'].username
+    }
+    r = client.post('/reset_credentials/username', data=params)
+    assert r.status_code == 302
+
