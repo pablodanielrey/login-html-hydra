f909e2ea66228cd37418b76aec2af5b127ef9a92
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-08-06 12:45:06 -0300

    agrego el validador de longitud de la clave

--- src/login_html_hydra/web/reset_credentials/forms.py
+++ src/login_html_hydra/web/reset_credentials/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class InputCode(FlaskForm):
     code = StringField('Código', validators=[DataRequired()])
 
 class InputCredentials(FlaskForm):
-    password = PasswordField('Nueva Contraseña', validators=[DataRequired()])
-    password2 = PasswordField('Repetir Contraseña', validators=[DataRequired()])
+    password = PasswordField('Nueva Contraseña', validators=[DataRequired(), Length(min=8)])
+    password2 = PasswordField('Repetir Contraseña', validators=[DataRequired(), Length(min=8)])
 
 class InputUsername(FlaskForm):
     username = StringField('Ingrese su DNI', validators=[DataRequired()])
