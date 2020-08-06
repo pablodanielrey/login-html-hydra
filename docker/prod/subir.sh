3b28f109719ad348651ed3f3652f2ca7b8319cc3
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-08-06 13:51:18 -0300

    actualizo la version

--- docker/prod/subir.sh
+++ docker/prod/subir.sh
@@ -1,4 +1,4 @@
-docker tag login-html-hydra-prod registry.econo.unlp.edu.ar/login-html-hydra-prod:v1.1.2
-docker push registry.econo.unlp.edu.ar/login-html-hydra-prod:v1.1.2
+docker tag login-html-hydra-prod registry.econo.unlp.edu.ar/login-html-hydra-prod:v1.1.3
+docker push registry.econo.unlp.edu.ar/login-html-hydra-prod:v1.1.3
 docker tag login-html-hydra-prod registry.econo.unlp.edu.ar/login-html-hydra-prod:latest
 docker push registry.econo.unlp.edu.ar/login-html-hydra-prod:latest
